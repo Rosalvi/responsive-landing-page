4ab23f10db4b905b4660769d388ee5c59461d8b5
Rosalvi <rosi2194@gmail.com>
2025-03-27 13:45:16 +0100

    adapt box for image

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,12 @@ function loadComponent(containerId, filePath) {
 document.addEventListener("DOMContentLoaded", () => {
     loadComponent("header-container", "components/header.html");
     loadComponent("hero-container", "components/hero-section.html");
+    const menuBtn = document.querySelector(".hamburger-menu");
+    const navContainer = document.querySelector(".nav-container");
+
+    menuBtn.addEventListener("click", function () {
+        navContainer.classList.toggle("active");
+    });
+
+
 });
