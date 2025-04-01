3117a538b5a1142ba6c0c2c1f7bcdfc8c82a95c8
Rosalvi <rosi2194@gmail.com>
2025-04-01 13:28:58 +0200

    add responsive to hero section and modularixed css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,40 @@
-function loadComponent(containerId, filePath) {
+function loadComponent(containerId, filePath, callback) {
     fetch(filePath)
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error loading ${filePath}: ${response.statusText}`);
+            }
+            return response.text();
+        }
+        )
         .then(html => {
             document.getElementById(containerId).innerHTML = html;
+            if (callback) {
+                callback();
+            }
         })
         .catch(error => console.error(`Error cargando ${filePath}:`, error));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    loadComponent("header-container", "components/header.html");
+    loadComponent("header-container", "components/header.html", initializeMenu);
     loadComponent("hero-container", "components/hero-section.html");
-    const menuBtn = document.querySelector(".hamburger-menu");
-    const navContainer = document.querySelector(".nav-container");
+    loadComponent("features-container", "components/features.html");
 
+    function initializeMenu() {
+        const menuBtn = document.querySelector(".hamburger-menu");
+        const navContainer = document.querySelector(".nav-container");
+
+        if (menuBtn && navContainer) {
+            menuBtn.addEventListener("click", function () {
+                navContainer.classList.toggle("active");
+            });
+        } else {
+            console.error("Menu button or nav container not found.");
+        }
+
+    }
+    
     menuBtn.addEventListener("click", function () {
         navContainer.classList.toggle("active");
     });
